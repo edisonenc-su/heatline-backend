9318bf5e6ed43a571daf3903ec4f8d43ce2bf4b0
edisonenc-su <hello@edisonenc.com>
2026-04-06 10:42:08 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 const app = require('./app');
 const env = require('./config/env');
 const db = require('./config/db');
+const { startWeatherAutoScheduler, stopWeatherAutoScheduler } = require('./services/weather-auto');
 
 async function bootstrap() {
   try {
     await db.ping();
-    app.listen(env.port, () => {
+    await startWeatherAutoScheduler();
+
+    const server = app.listen(env.port, () => {
       console.log(`[heatline-central-backend] listening on port ${env.port}`);
     });
+
+    const shutdown = () => {
+      stopWeatherAutoScheduler();
+      server.close(() => process.exit(0));
+    };
+
+    process.on('SIGINT', shutdown);
+    process.on('SIGTERM', shutdown);
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
